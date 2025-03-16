8c36f437529efb70cb7923e7af1bf1cce31ba491
Daniel Escanez-Exposito <jescanez@ull.edu.es>
2025-03-16 17:18:14 +0000

    Add arguments parser

--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,62 @@ use std::fs;
 use std::fs::File;
 use std::io::Write;
 
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the file with the initial generation
+    #[arg(short, long)]
+    input_filename: std::path::PathBuf,
+    /// Name of the file to save the memory of the game
+    #[arg(short, long)]
+    output_filename: std::path::PathBuf,
+    /// Numbers of turns that will be played
+    #[arg(short, long)]
+    turns: usize,
+    /// Set of alive neighbourhoods sizes to survive in the next turn
+    #[arg(short, long, default_value_t = ("[2, 3]").to_string())]
+    survivals: String,
+    /// Set of alive neighbourhoods sizes to revive in the next turn
+    #[arg(short, long, default_value_t = ("[3]").to_string())]
+    revivals: String,
+}
+
 fn main() {
-    let input_filename = std::env::args().nth(1).unwrap_or_else(|| {
-        eprintln!("Please, provide the input file as first argument");
-        std::process::exit(1)
-    });
-
-    let output_filename = std::env::args().nth(2).unwrap_or_else(|| {
-        eprintln!("Please, provide the output file as second argument");
-        std::process::exit(2)
-    });
-
-    let turns = std::env::args()
-        .nth(3)
-        .unwrap_or_else(|| {
-            eprintln!("Please, provide the number of turns as third argument");
-            std::process::exit(3)
-        })
-        .parse::<usize>()
-        .unwrap_or_else(|_| {
-            eprintln!("The number of turns must be a positive integer");
-            std::process::exit(4)
-        });
+    let args = Args::parse();
 
     let board_str =
-        fs::read_to_string(input_filename).expect("Should have been able to read the file");
+        fs::read_to_string(args.input_filename).expect("Should have been able to read the file");
     let (_, mut board) = read_input(&board_str).unwrap();
 
-    for _ in 0..turns {
+    board.change_rules((
+        ron::from_str(&args.survivals).unwrap_or_else(|_| {
+            eprintln!("Please, provide the survivals in a serializable Vec<usize> way");
+            std::process::exit(1)
+        }),
+        ron::from_str(&args.revivals).unwrap_or_else(|_| {
+            eprintln!("Please, provide the revivals in a serializable Vec<usize> way");
+            std::process::exit(1)
+        }),
+    ));
+
+    for _ in 0..args.turns {
         board.next();
     }
 
-    let mut output_file = File::create(output_filename).unwrap();
+    let mut output_file = File::create(args.output_filename).unwrap();
 
     let output = board
         .get_memory()
         .iter()
         .map(|turn| {
             turn.iter()
-                .map(|row| row.iter().map(|cell| cell.prob_alive.to_string()).collect())
+                .map(|row| row.iter().map(|cell| cell.prob_alive).collect())
                 .collect()
         })
-        .collect::<Vec<Vec<Vec<String>>>>();
+        .collect::<Vec<Vec<Vec<f64>>>>();
 
     output_file
         .write_all(ron::to_string(&output).unwrap().as_bytes())
